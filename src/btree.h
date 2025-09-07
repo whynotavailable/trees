dd2b2f427adc9293f9402195bca37811f3c750b4
Tom <josephdotson@belkonar.com>
2025-09-06 21:26:42 -0400

    destructors

--- src/btree.h
+++ src/btree.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <vector>
 namespace BTree {
 
 enum BTreeStatus {
     Ok,
+    OverCapacity,
     UndefinedError,
 };
 
@@ -14,6 +16,31 @@ struct BTreeConfig {
 class BTreeNode {
 private:
     BTreeConfig config;
+    std::vector<int> keys;
+    std::vector<BTreeNode *> children;
+
+public:
+    int depth; // This works in reverse, so a leaf will be 0
+
+    BTreeNode(BTreeConfig config, int depth) {
+        this->config = config;
+        this->depth = depth;
+    }
+
+    // TODO: Need to test, but it should clear everything down the tree.
+    ~BTreeNode() {
+        for (auto it = this->children.begin(); it != this->children.end();
+             it++) {
+            delete *it;
+            *it = 0;
+        }
+
+        this->children.clear();
+    }
+
+    BTreeStatus Add(int key) {
+        return BTreeStatus::Ok;
+    }
 };
 
 class BTree {
@@ -21,8 +48,18 @@ private:
     BTreeConfig config;
 
 public:
+    BTreeNode *root = 0;
+
     BTree() {
         config.maxKeys = 4;
+        this->root = new BTreeNode(this->config, 0);
+    }
+
+    ~BTree() {
+        if (this->root != 0) {
+            delete this->root;
+            this->root = 0;
+        }
     }
 
     BTree(BTreeConfig config) {
@@ -31,6 +68,10 @@ public:
 
     BTreeStatus Add(int key) {
         return BTreeStatus::Ok;
+    }
+
+    BTreeNode NewNode(BTreeConfig config, int depth) {
+        return BTreeNode(config, depth);
     }
 };
 
